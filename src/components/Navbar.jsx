3046e0ef317ac2dad2e8062316f1cbb87342cedd
nehal-5 <nehalfathema08@gmail.com>
2025-04-13 13:54:40 +0530

    Added a responsive hamburger for phone view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import '../styles/Navbar.css';
+import { useState } from "react";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
     return (
         <header>
             <nav className="navBar">
                 <div className="vclogo">
                     <img src="/assets/vcLogo.png" alt="Vision Care Opticals Logo Image" />
                 </div>
-                <ul className="navLinks">
+                <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className={`navLinks ${isOpen ? "open" : ""}`}>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#products">Products</a></li>
                     <li><a href="#services">Services</a></li>
