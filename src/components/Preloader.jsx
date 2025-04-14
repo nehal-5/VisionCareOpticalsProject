75c87f82503ca71c88fb57aeaf62248a1bd72f01
nehal-5 <nehalfathema08@gmail.com>
2025-04-14 10:46:43 +0530

    preloader.jsx file added

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,14 @@
+// src/components/Preloader.jsx
+import React from 'react';
+import '../styles/Preloader.css';
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <img src="/assets/loadingImage.png" alt="Loading" className="spin-logo" />
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Preloader;
